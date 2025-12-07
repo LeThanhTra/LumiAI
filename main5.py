28b7c1bbfab85687915e756da1cb5b38766ba498
LeThanhTra <157978030+LeThanhTra@users.noreply.github.com>
2025-12-07 19:18:11 +0700

    Add files via upload

--- main5.py
+++ main5.py
@@ -1,16 +1,6 @@
 # -----------------------------
 # 1. Kết nối API
 # -----------------------------
-from openai import OpenAI
-os.environ["OPENAI_API_BASE"] = "https://openrouter.ai/api/v1"
-os.environ["OPENAI_API_KEY"] = "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"
-
-client = OpenAI()
-#client = OpenAI(
-   # base_url="https://openrouter.ai/api/v1",
-   # api_key="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"
-#)
-
 import pandas as pd
 import streamlit as st
 import faiss
@@ -20,6 +10,16 @@ import json
 import os
 import unicodedata
 import re
+
+from openai import OpenAI
+os.environ["OPENAI_API_BASE"] = "https://openrouter.ai/api/v1"
+os.environ["OPENAI_API_KEY"] = "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"
+
+client = OpenAI()
+#client = OpenAI(
+   # base_url="https://openrouter.ai/api/v1",
+   # api_key="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"
+#)
 
 
 # -----------------------------
